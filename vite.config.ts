9e4b36a212f2562c484be3d78f30a1c8cb8b6c83
Adedamola Egbebi <egbebitimi121@gmail.com>
2026-01-13 09:50:01 +0100

    Add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -42,50 +44,17 @@ export default defineConfig({
     // This ensures Vite runs on the expected port
     port: 5173,
     strictPort: true,
+  },
+  test: {
+    environment: 'node',
+    clearMocks: true,
+  },
+  ssr: {
+    noExternal: true,
+  },
+  resolve: {
+    conditions: ['node', 'import', 'module', 'browser', 'default'],
   }
 })
 
-/*
-{
-  "name": "biz_hq_1",
-  "private": true,
-  "version": "0.0.0",
-  "type": "module",
-  "main": "src/main.cjs",
-  "scripts": {
-    "dev": "concurrently \"npm run dev:react\" \"npm run dev:electron\"",
-    "dev:react": "vite",
-    "dev:electron": "wait-on http://localhost:5173 && electron .",
-    "build": "tsc && vite build && electron-builder",
-    "preview": "vite preview"
-  },
-  "dependencies": {
-    "react": "^19.2.0",
-    "react-dom": "^19.2.0",
-    "recharts": "^3.6.0"
-  },
-  "devDependencies": {
-    "@eslint/js": "^9.39.1",
-    "@types/node": "^24.10.1",
-    "@types/react": "^19.2.5",
-    "@types/react-dom": "^19.2.3",
-    "@vitejs/plugin-react": "^5.1.1",
-    "concurrently": "^9.2.1",
-    "electron": "^39.2.7",
-    "electron-builder": "^26.0.12",
-    "eslint": "^9.39.1",
-    "eslint-plugin-react-hooks": "^7.0.1",
-    "eslint-plugin-react-refresh": "^0.4.24",
-    "globals": "^16.5.0",
-    "typescript": "~5.9.3",
-    "typescript-eslint": "^8.46.4",
-    "vite": "^7.2.4",
-    "vite-plugin-electron": "^0.29.0",
-    "vite-plugin-electron-renderer": "^0.14.6",
-    "wait-on": "^9.0.3"
-  }
-}
-
-
-*/
 
